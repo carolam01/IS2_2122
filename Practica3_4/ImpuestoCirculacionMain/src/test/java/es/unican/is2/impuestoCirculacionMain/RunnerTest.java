ab7506835334650aff71e32d27dbd8b334e40371
Alejandro <alsasou8@gmail.com>
2022-04-17 17:49:19 +0200

    Ejercicio 2 tambien

--- Practica3_4/ImpuestoCirculacionMain/src/test/java/es/unican/is2/impuestoCirculacionMain/RunnerTest.java
+++ Practica3_4/ImpuestoCirculacionMain/src/test/java/es/unican/is2/impuestoCirculacionMain/RunnerTest.java
@@ -68,7 +68,6 @@ public class RunnerTest {
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -99,7 +98,6 @@ public class RunnerTest {
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -129,7 +127,6 @@ public class RunnerTest {
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
